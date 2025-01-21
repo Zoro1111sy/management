232538b14d85a2e8d10dd001344406c72bfff3ab
sy_zoro_1a038 <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>
2025-01-21 19:44:07 +0900

    Initial Commit

---
+++ management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  management
+//
+//  Created by Sae Yamauchi on 2025/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
